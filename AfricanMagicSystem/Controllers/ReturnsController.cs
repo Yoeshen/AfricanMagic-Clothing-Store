bb04a0239f84482c697a9da42b4f17983205f939
Yoeshen Govender <yoeshen@gmail.com>
2020-08-09 20:51:39 +0200

    ReturnsController

---
+++ AfricanMagicSystem/Controllers/ReturnsController.cs
@@ -0,0 +1,34 @@
+﻿using AfricanMagicSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AfricanMagicSystem.Controllers
+{
+    public class ReturnsController : Controller
+    {
+        private ApplicationDbContext dB = new ApplicationDbContext();
+
+        public ActionResult SaleValidCheck(int? saleid)
+        {
+            List<Sale> chck = (from x in dB.Sales
+                               select x).ToList();
+            DateTime currentDate = DateTime.Now;
+
+
+            foreach (var x in chck)
+            {
+                DateTime SaleDate = x.SaleDate;
+                TimeSpan diff = currentDate.Subtract(SaleDate);
+
+                if (saleid == x.SaleId && diff.TotalDays < 7)
+                {
+
+                }
+            }
+        }
+    }
+}
