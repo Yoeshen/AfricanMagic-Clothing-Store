309f81a1c27d776e8aa4842cd0ce0745b20adb05
Yoeshen Govender <yoeshen@gmail.com>
2020-08-09 21:05:08 +0200

    Return Controller V2

--- AfricanMagicSystem/Controllers/ReturnsController.cs
+++ AfricanMagicSystem/Controllers/ReturnsController.cs
@@ -12,23 +12,27 @@ namespace AfricanMagicSystem.Controllers
     {
         private ApplicationDbContext dB = new ApplicationDbContext();
 
-        public ActionResult SaleValidCheck(int? saleid)
+        public async Task<ActionResult> SaleValidCheck(ReturnItem returnItem)
         {
             List<Sale> chck = (from x in dB.Sales
                                select x).ToList();
             DateTime currentDate = DateTime.Now;
-
-
-            foreach (var x in chck)
+            if (ModelState.IsValid)
             {
-                DateTime SaleDate = x.SaleDate;
-                TimeSpan diff = currentDate.Subtract(SaleDate);
-
-                if (saleid == x.SaleId && diff.TotalDays < 7)
+                foreach (var x in chck)
                 {
+                    DateTime SaleDate = x.SaleDate;
+                    TimeSpan diff = currentDate.Subtract(SaleDate);
 
+                    if (returnItem.SaleID == x.SaleId && diff.TotalDays < 7)
+                    {
+                        dB.ReturnItems.Add(returnItem);
+                        await dB.SaveChangesAsync();
+                        return View("Success");
+                    }
                 }
             }
-        }
+            return View("Invalid");
+        } 
     }
 }
