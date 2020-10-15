fcd06810dcced1bb00eeed15531c8963d005e830
Atish Krishen Hariparsad <21825470@dut4life.ac.za>
2020-10-15 12:14:32 +0200

    AddedTracking

---
+++ AfricanMagicSystem/Controllers/TrackingController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AfricanMagicSystem.Models;
+using Microsoft.AspNet.Identity;
+
+namespace AfricanMagicSystem.Controllers
+{
+    public class TrackingController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Tracking
+        public ActionResult Index()
+        {
+            var sale = db.Sales.Where(s => s.Username == User.Identity.Name);
+            return View(sale);
+
+        }
+
+        // GET: Tracking/Details/5
+        public ActionResult Details(int id)
+        {
+            Delivery delivery = db.Deliveries.Find(id);
+
+            Product product = db.Products.Find(id);
+
+            var orderDetails = db.SalesDetails.Where(x => x.ProductId == product.ID);
+                       
+            List<string> ListOfItems = new List<string>();
+            foreach (var r in orderDetails)
+            {
+                ListOfItems.Add(r.Product.Name);
+            }
+
+            ViewBag.Products = ListOfItems;
+
+            return View(delivery);
+        }
+
+
+    }
+}
