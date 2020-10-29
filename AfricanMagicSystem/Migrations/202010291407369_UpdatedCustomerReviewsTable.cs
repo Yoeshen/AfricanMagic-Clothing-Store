2485ac4787c7cfab2199aec59c8e49251732286b
Yoeshen Govender <yoeshen@gmail.com>
2020-10-29 16:09:00 +0200

    UpdatedCustomerReviews

---
+++ AfricanMagicSystem/Migrations/202010291407369_UpdatedCustomerReviewsTable.cs
@@ -0,0 +1,32 @@
+﻿namespace AfricanMagicSystem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdatedCustomerReviewsTable : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.CustomerReviews", "SaleID", "dbo.Sales");
+            DropIndex("dbo.CustomerReviews", new[] { "SaleID" });
+            AddColumn("dbo.Sales", "CustomerReviews_CustomerReviewID", c => c.Int());
+            AddColumn("dbo.CustomerReviews", "InvoiceNumber", c => c.Int(nullable: false));
+            AlterColumn("dbo.CustomerReviews", "Comment", c => c.String(nullable: false));
+            CreateIndex("dbo.Sales", "CustomerReviews_CustomerReviewID");
+            AddForeignKey("dbo.Sales", "CustomerReviews_CustomerReviewID", "dbo.CustomerReviews", "CustomerReviewID");
+            DropColumn("dbo.CustomerReviews", "SaleID");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.CustomerReviews", "SaleID", c => c.Int(nullable: false));
+            DropForeignKey("dbo.Sales", "CustomerReviews_CustomerReviewID", "dbo.CustomerReviews");
+            DropIndex("dbo.Sales", new[] { "CustomerReviews_CustomerReviewID" });
+            AlterColumn("dbo.CustomerReviews", "Comment", c => c.String());
+            DropColumn("dbo.CustomerReviews", "InvoiceNumber");
+            DropColumn("dbo.Sales", "CustomerReviews_CustomerReviewID");
+            CreateIndex("dbo.CustomerReviews", "SaleID");
+            AddForeignKey("dbo.CustomerReviews", "SaleID", "dbo.Sales", "SaleId", cascadeDelete: true);
+        }
+    }
+}
--- AfricanMagicSystem/Models/CustomerReviews.cs
+++ AfricanMagicSystem/Models/CustomerReviews.cs
@@ -13,19 +13,23 @@ namespace AfricanMagicSystem.Models
         [Key]
         public int CustomerReviewID { get; set; }
 
-        public int SaleID { get; set; }
+        [Required(ErrorMessage = "Please enter a valid Sale ID. Find this on your Invoice.")]
+        [RegularExpression(@"^[0-9]+$", ErrorMessage = "Please use numbers only")]
+        public int InvoiceNumber { get; set; }
 
         public string Username { get; set; }
 
         public int Vote { get; set; }
 
+        [Required(ErrorMessage = "Enter a comment.")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Please use letters only")]
         public string Comment { get; set; }
 
         public bool Flagged { get; set; }
 
         public bool Approved { get; set; }
 
-        public virtual Sale Sale { get; set; }
+        public List<Sale> sales { get; set; }
 
 
     }
